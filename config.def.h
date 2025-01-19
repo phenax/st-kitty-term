fdfc75026050a5f0493693f9f8355c2baf9c7be2
Akshay Nair <phenax5@gmail.com>
2025-01-19 16:58:01 +0530

    Update font

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /* How to align the content in the window when the size of the terminal

3f19e6b7d142162ba9fe5b8c84fed38e196c9bac
Akshay Nair <phenax5@gmail.com>
2025-07-01 00:09:49 +0530

    Update xresources.h

--- xres.sh
+++ xres.sh
@@ -2,8 +2,16 @@
 
 set -e -o pipefail
 
+cmd_exists() { type "$1" > /dev/null; }
+
 xres() {
-  xrdb -q;
+  local xres_nix="../../../modules/xresources.home.nix"
+  if [ -f "$xres_nix" ] && cmd_exists nix; then
+    nix eval --impure --json --expr "(import $xres_nix {}).xresources.properties" | \
+      jq -r 'to_entries | map(.key + ": " + .value) | join("\n")'
+  else
+    xrdb -q;
+  fi
 }
 
 to_c_source() {
